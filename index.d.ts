efe69da9747afa0bb3c3d528afca9e884c9ea05f
Mike <mikemingway@gmail.com>
2021-10-19 21:27:23 -0400

    Create index.d.ts
    Facilitates type discovery and code completion

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import UnityView from "./src/UnityView";
+import { UnityModule, UnityViewMessage } from "./src/UnityModule";
+import MessageHandler from "./src/MessageHandler";
+import UnityResponderView from './src/UnityResponderView';
+
+export default UnityView;
+
+export {
+  UnityModule,
+  MessageHandler,
+  UnityViewMessage,
+  UnityResponderView
+};
